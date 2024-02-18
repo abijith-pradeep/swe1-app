6e3b3287bf88e8a6da8613e1a103b2c0e5805432
Abijith Pradeep <321niugnep@gmail.com>
2024-02-18 12:23:50 -0500

    First Personal Assignment- Polls

---
+++ assignments/ap8246_polls/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Ap8246PollsConfig(AppConfig):
+    name = 'ap8246_polls'
---
+++ swe1-virt/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\321ni\Desktop\Year 2023- 2024\swe1-app\swe1-virt\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
